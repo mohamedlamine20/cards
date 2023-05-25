0249e972b151fb26661b558ad961ff625acabe7b
mohamed <mlamine642@gmail.com>
2023-05-25 20:53:26 +0100

    create cards app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts =[
+    {
+      title:"Neat Tree",
+      username:"nature",
+      imageUrl:"assets/tree.jpeg",
+      content:" i saw this neat tree tody"
+
+    },
+    {
+      title:"Snowy mountain",
+      username:"mountainlover",
+      imageUrl:"assets/mountain.jpeg",
+      content:"here is a picture of a snowy mountain "
+    },
+    {
+      title:"mountain biking",
+      username:"biking ",
+      imageUrl:"assets/biking.jpeg",
+      content:"idid same biking today "
+       
+    }
+  ]; 
 }
